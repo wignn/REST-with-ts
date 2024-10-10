229d30182600c49e87f5da907bafdf74c13e108e
wignn <tigfiragnafatur1933@gmail.com>
2024-10-10 08:41:13 +0700

    refactor: Update user-service.ts for user registration validation
    Refactor the user-service.ts file to update the user registration validation process. This includes making use of the UserValidation.REGISTER validation schema and the validation.validate() function. Additionally, the code now checks for existing users with the same username or email using the prismaClient.user.count() function. If there are any existing users, an error is thrown.

--- src/service/user-service.ts
+++ src/service/user-service.ts
@@ -1,9 +1,27 @@
-import { UserValidation } from './../validation/user-validation';
+import { UserValidation } from "./../validation/user-validation";
 import { validation } from "./../validation/validation";
 import { CreateUserRequest, UserRessponse } from "../model/user-mode";
+import { prismaClient } from "../application/database";
 
 export class UserService {
   static async register(request: CreateUserRequest): Promise<UserRessponse> {
-    const registerRequest = validation.validate(UserValidation.REGISTER, request);
+    const registerRequest = validation.validate(
+      UserValidation.REGISTER,
+      request
+    );
+    const totalUsersWithUsername = await prismaClient.user.count({
+      where: {
+        username: registerRequest.username,
+      },
+    });
+    const totalUsersWithEmail = await prismaClient.user.count({
+      where: {
+        email: registerRequest.email,
+      },
+    });
+
+    if (totalUsersWithUsername || totalUsersWithEmail) {
+      throw new Error
+    }
   }
 }
