0ed62d93ba74a424f9a4f3d1c1fa05a704915f59
wignn <tigfiragnafatur1933@gmail.com>
2024-10-10 09:26:48 +0700

    refactor: Update package dependencies and add types for bcrypt and jest

---
+++ test/user.test.ts
@@ -0,0 +1,20 @@
+import { logger } from '../src/application/loggin';
+import { web } from './../src/application/web';
+import supertest from "supertest"
+
+describe('POST /api/users', () => {
+    it('should reject register new user if request is invali', async () => {
+        const response = await supertest(web)
+        .post('/api/users')
+        .send({
+            username: 'mjkd',
+            password: '',
+            email: '',
+            name: ''
+        })
+        logger.debug(response.body);
+        expect(response.status).toBe(400);
+        expect(response.body).toBeDefined();
+    })
+   
+})
